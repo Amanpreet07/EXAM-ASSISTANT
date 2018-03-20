210921c57efc523940fa37468d32d35c7d690612
Amanpreet07 <arora07aman@gmail.com>
2018-03-20 15:56:50 +0530

    initial setup, added lib

--- src/exam/assistant/ExamAssistant.java
+++ src/exam/assistant/ExamAssistant.java
@@ -1,5 +1,6 @@
 package exam.assistant;
 
+import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -9,7 +10,7 @@ import javafx.stage.Stage;
 public class ExamAssistant extends Application {
     
     @Override
-    public void start(Stage stage) {
+    public void start(Stage stage) throws IOException {
          Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
         Scene scene = new Scene(root);
         stage.setScene(scene);
