e74e539c80ce9a10947e30c69b0798adc05a6404
Amanpreet07 <arora07aman@gmail.com>
2018-03-22 23:00:04 +0530

    added security questions

--- src/core/user_manager/meta.java
+++ src/core/user_manager/meta.java
@@ -7,7 +7,34 @@ public class meta {
     private String securityAnswer;
     private String securityQuestion;
     private String email;
+    
+    private final String[] ques = {       
+    "Which phone number do you remember most from your childhood?",
+    "What was your favorite place to visit as a child?",
+    "Who is your favorite actor, musician, or artist?",
+    "What is the name of your favorite pet?",
+    "In what city were you born?",
+    "What high school did you attend?",
+    "What is the name of your first school?",
+    "What is your favorite movie?",
+    "What is your mother's maiden name?",
+    "What was the make of your first car?",
+    "When is your anniversary?",
+    "What is your favorite color?",
+    "What is your father's middle name?",
+    "What was your high school mascot?",
+    "Which is your favorite web browser?",
+    "what is your favorite website?",
+    "what is your favorite forum?",
+    "what is your favorite online platform?",
+    "what is your favorite social media website?"
+    };
 
+    public String[] getQues() {
+        return ques;
+    }
+    
+    
     public String getUsername() {
         return username;
     }
